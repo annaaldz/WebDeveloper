2a6df26bc3ca9e7a69462afb21114ca921ca74ff
Anna Carolina <101186654+annaaldz@users.noreply.github.com>
2022-10-14 13:35:29 -0300

    Update index.php

--- index.php
+++ index.php
@@ -13,36 +13,49 @@
   <script src="css_js/bootstrap-4.1.3/js/bootstrap.min.js"></script>
 
 <script>
-function validaCPF($cpf) {
+	function validaCPF($cpf) {
 
-  var cpf= document.getElementById("cpf");
- 
-    // Extrai somente os números
-    $cpf = preg_replace( '/[^0-9]/is', '', $cpf );
-     
-    // Verifica se foi informado todos os digitos corretamente
-    if (strlen($cpf) != 11) {
-        return false;
-    }
+	  var cpf= document.getElementById("cpf");
 
-    // Verifica se foi informada uma sequência de digitos repetidos. Ex: 111.111.111-11
-    if (preg_match('/(\d)\1{10}/', $cpf)) {
-        return false;
-    }
+	    // Extrai somente os números
+	    $cpf = preg_replace( '/[^0-9]/is', '', $cpf );
 
-    // Faz o calculo para validar o CPF
-    for ($t = 9; $t < 11; $t++) {
-        for ($d = 0, $c = 0; $c < $t; $c++) {
-            $d += $cpf[$c] * (($t + 1) - $c);
-        }
-        $d = ((10 * $d) % 11) % 10;
-        if ($cpf[$c] != $d) {
-            return false;
-        }
-    }
-    return true;
-	
-	
+	    // Verifica se foi informado todos os digitos corretamente
+	    if (strlen($cpf) != 11) {
+		return false;
+	    }
+
+	    // Verifica se foi informada uma sequência de digitos repetidos. Ex: 111.111.111-11
+	    if (preg_match('/(\d)\1{10}/', $cpf)) {
+		return false;
+	    }
+
+	    // Faz o calculo para validar o CPF
+	    for ($t = 9; $t < 11; $t++) {
+		for ($d = 0, $c = 0; $c < $t; $c++) {
+		    $d += $cpf[$c] * (($t + 1) - $c);
+		}
+		$d = ((10 * $d) % 11) % 10;
+		if ($cpf[$c] != $d) {
+		    return false;
+		}
+	    }
+	    return true;
+		
+	function validaEMAIL($email){
+	var email = document.getElementById("email");
+
+	function validateEmail(email) {
+	  var email = /\S+@\S+\.\S+/;
+	  return email.test(email);
+	}
+
+	console.log(validateEmail('texto@texto.com')); // true
+	console.log(validateEmail('texto@texto')); // false
+	console.log(validateEmail('texto.com')); // false
+	console.log(validateEmail('texto')); // false	
+	}
+		
 </script>
 
 
